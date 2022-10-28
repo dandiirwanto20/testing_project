2428a49f20890a23488d19e9a99a68826372336c
Dandi <dandi.ir20@gmail.com>
2022-10-28 23:14:27 +0700

    xxx

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -2,7 +2,7 @@ import UrlParser from '../../routes/url-parser';
 import RestaurantSource from '../../data/restaurant-source';
 import { restaurantDetailTemplate } from '../templates/template-html';
 import LikeButtonPresenter from '../../utils/like-button-presenter';
-import PostReview from '../../utils/post-review';
+// import PostReview from '../../utils/post-review';
 import Spinner from '../templates/spinner-html';
 import FavRestaurantIdb from '../../data/restaurant-idb';
 
@@ -15,19 +15,19 @@ const Detail = {
         <h2 class="title-container">Detail Restaurant</h2>
         <section id="detail-rest"></section>
         <div class="like" id="likeButtonContainer"></div>
-        <div class="form-review">
-          <form>
-            <div class="mb-3">
-              <label for="inputName" class="form-label">Name</label>
-              <input name="inputName" type="text" class="form-control" id="inputName">
-            </div>
-            <div class="mb-3">
-              <label for="inputReview" class="form-label">Review</label>
-              <input name="inputReview" type="text" class="form-control" id="inputReview">
-            </div>
-            <button id="submit-review" type="submit" class="btn2">Submit</button>
-          </form>
-        </div>
+        // <div class="form-review">
+        //   <form>
+        //     <div class="mb-3">
+        //       <label for="inputName" class="form-label">Name</label>
+        //       <input name="inputName" type="text" class="form-control" id="inputName">
+        //     </div>
+        //     <div class="mb-3">
+        //       <label for="inputReview" class="form-label">Review</label>
+        //       <input name="inputReview" type="text" class="form-control" id="inputReview">
+        //     </div>
+        //     <button id="submit-review" type="submit" class="btn2">Submit</button>
+        //   </form>
+        // </div>
       </div>
     </div>
     `;
@@ -59,23 +59,23 @@ const Detail = {
       loading.style.display = 'none';
     }
 
-    const btnSubmit = document.querySelector('#submit-review');
-    const nameInput = document.querySelector('#inputName');
-    const reviewInput = document.querySelector('#inputReview');
+    // const btnSubmit = document.querySelector('#submit-review');
+    // const nameInput = document.querySelector('#inputName');
+    // const reviewInput = document.querySelector('#inputReview');
 
-    btnSubmit.addEventListener('click', (e) => {
-      e.preventDefault();
-      if (nameInput.value === '' || reviewInput.value === '') {
-        // eslint-disable-next-line no-alert
-        alert('Inputan tidak boleh ada yang kosong');
-        nameInput.value = '';
-        reviewInput.value = '';
-      } else {
-        PostReview(url, nameInput.value, reviewInput.value);
-        nameInput.value = '';
-        reviewInput.value = '';
-      }
-    });
+    // btnSubmit.addEventListener('click', (e) => {
+    //   e.preventDefault();
+    //   if (nameInput.value === '' || reviewInput.value === '') {
+    //     // eslint-disable-next-line no-alert
+    //     alert('Inputan tidak boleh ada yang kosong');
+    //     nameInput.value = '';
+    //     reviewInput.value = '';
+    //   } else {
+    //     PostReview(url, nameInput.value, reviewInput.value);
+    //     nameInput.value = '';
+    //     reviewInput.value = '';
+    //   }
+    // });
   },
 };
 
